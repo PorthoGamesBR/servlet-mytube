d0fe4392f7dcb66d169e8843c2096d57270a508a
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2025-01-08 17:38:06 -0300

    Mudando para selecionar video por parametro

--- mytube/src/main/java/Videos.java
+++ mytube/src/main/java/Videos.java
@@ -4,12 +4,16 @@ import java.io.*;
 
 public class Videos extends HttpServlet {
 
+    // TODO: Pegar esse path de uma variavel configuravel pelo usuario, como variável de ambiente
     public String videoPath = "/home/portho/Downloads/";
 
+    @GetMapping("/videos")
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String videoName = request.getQueryString()
+
         response.setContentType("video/mp4");
         ServletOutputStream out = response.getOutputStream();
-        FileInputStream fin = new FileInputStream(videoPath + "video.mp4");
+        FileInputStream fin = new FileInputStream(videoPath + videoName);
 
         byte [] buf = new byte[4096];
         int read;
